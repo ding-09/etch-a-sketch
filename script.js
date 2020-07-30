92ccd52d5be1cfb5d6969a1b4e5287f9485a6633
tiffany <yang.ding@hotmail.com>
2020-07-29 17:02:40 -0700

    etch-a-sketch v2 with clear and reset buttons added

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 let grid = document.querySelector('.grid-container');
 let gridSquares = grid.children;
-// create a 16 x 16 grid 
-// 16 rows x 16 columns
 
-grid.style.gridTemplateColumns = "repeat(16, auto)";
-grid.style.gridTemplateRows = "repeat(16, auto)";
+// initialize a 16 x 16 grid 
 
-for (i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('grid-square-style');
-    grid.appendChild(div);
+function generateGrid(){
+    grid.style.gridTemplateColumns = "repeat(16, auto)";
+    grid.style.gridTemplateRows = "repeat(16, auto)";
+
+    for (i = 0; i < 256; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-square-style');
+        grid.appendChild(div);
+    }
+
+    // add event listener to each grid square 
+    for (i = 0; i < 256; i++) {
+        gridSquares[i].addEventListener('mouseover', addRandomColor);
+    }
 }
+
+generateGrid();
+
+// generate random rgb values
 
 function generateRandomNumber() {
     return Math.floor(Math.random() * (256 - 0 + 1)) + 0;
@@ -28,21 +39,55 @@ function generateRandomB() {
     return generateRandomNumber();
 }
 
-function generateRandomA() {
-    return Number(Math.random().toFixed(1));
-}
-
-
 function addRandomColor(e) {
     let r = generateRandomR();
     let g = generateRandomG();
     let b = generateRandomB();
-    let a = generateRandomA();
-
-    e.target.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+    e.target.style.backgroundColor = `rgba(${r}, ${g}, ${b})`;
 }
 
-// add event listener to each grid square 
-for (i = 0; i < 256; i++) {
-    gridSquares[i].addEventListener('mouseover', addRandomColor);
+// reset grid when clear button is clicked 
+let clearBtn = document.getElementById('clear');
+
+function clearGrid() {
+    for (i = 0; i < 256; i++) {
+        gridSquares[i].style.backgroundColor = "white";
+    }
 }
+
+// add event listener to clear the grid on click 
+clearBtn.addEventListener('click', clearGrid);
+
+// resize grid when resize button is clicked 
+let resizeBtn = document.getElementById('resize');
+
+// helper function to reset grid to have 0 divs/children
+function resetGrid() {
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.lastChild);
+    }
+}
+
+function resizeGrid() {
+    resetGrid();
+
+    let newGridSize = prompt("Please enter a new grid size: ");
+    let gridTotal = newGridSize * newGridSize;
+
+    grid.style.gridTemplateColumns = `repeat(${newGridSize}, auto)`;
+    grid.style.gridTemplateRows = `repeat(${newGridSize}, auto)`;
+
+    for (i = 0; i < gridTotal; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-square-style');
+        grid.appendChild(div);
+    }  
+
+    // add event listener to each grid square 
+    for (i = 0; i < gridTotal; i++) {
+        gridSquares[i].addEventListener('mouseover', addRandomColor);
+    }
+}
+
+// add event listener to resize grid on click 
+resizeBtn.addEventListener('click', resizeGrid);
